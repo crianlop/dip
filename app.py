08eb5ee90b65a995ccee02e3fdcc8f36b71f0222
Carlos Moncayo <carlosamr2@hotmail.com>
2023-01-08 18:04:32 -0500

    ruido y filtro para sal y pimienta

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 from tkinter import filedialog
+import filterfunctions as fd
+import cv2
+import numpy as np
 root = tk.Tk()  # create root window
 root.title("Basic GUI Layout")  # title of the GUI window
 root.maxsize(900, 600)  # specify the max size the window can expand to
@@ -14,15 +17,20 @@ right_frame = tk.Frame(root, width=650, height=400, bg='grey')
 right_frame.grid(row=0, column=1, padx=10, pady=5)
 
 def loadPreview():
-    filename = filedialog.askopenfilename()
-    imagenOriginal=Image.open(filename)
-    imagenOriginal.thumbnail((350,350))
-    image = ImageTk.PhotoImage(imagenOriginal)
+    global image
+    global imagenOriginal
+    global filename
+    f_types = [('Archivos de imagen', ['*.jpg','*.jpeg','*.png'])]
+    filename = filedialog.askopenfilename(filetypes=f_types)
+    imagenOriginal= Image.open(filename)
+    copia=imagenOriginal.copy()
+    copia.thumbnail((350,350))
+    image = ImageTk.PhotoImage(copia)
     preview.configure(image=image)
     preview.image=image
-   
+
 # Create frames and labels in left_frame
-cargar=tk.Button(left_frame,text="Cargar imagen",command=loadPreview).grid(row=0, column=0, padx=5, pady=5)
+tk.Button(left_frame,text="Cargar imagen",command=lambda:loadPreview()).grid(row=0, column=0, padx=5, pady=5)
 
 # load image to be "edited"
 imagenOriginal=Image.open("imagen.jpg")
@@ -53,13 +61,45 @@ def aplicar():
     final.image=newImagen
 
 
+def aplicarsp():
+    copia=imagenOriginal.copy()
+    cvimage=np.array(copia)
+    cvimage=cvimage[:, :, ::-1].copy()
+    fd.saltPepper(cvimage)
+    name=filename.split("/")[-1]
+    newname="noisesp_"+name
+    cv2.imwrite(newname,cvimage)
+
+    img = cv2.cvtColor(cvimage, cv2.COLOR_BGR2RGB)
+    pilimg = Image.fromarray(img)
+    pilimg.thumbnail((350,350))
+    finalimg=ImageTk.PhotoImage(pilimg)
+    final.configure(image=finalimg)
+    final.image=finalimg
+
+def filtrosp():
+    name=filename.split("/")[-1]
+    newname="noisesp_"+name
+    restname="restoredsp_"+name
+    image=cv2.imread(newname)
+    copia=image.copy()
+    fd.saltPepperFilter(copia)
+
+    cv2.imwrite(restname,copia)
+    img = cv2.cvtColor(copia, cv2.COLOR_BGR2RGB)
+    pilimg = Image.fromarray(img)
+    pilimg.thumbnail((350,350))
+    finalimg=ImageTk.PhotoImage(pilimg)
+    final.configure(image=finalimg)
+    final.image=finalimg
 
 # Example labels that could be displayed under the "Tool" menu
-tk.Button(tool_bar, text="Sal y pimienta",command=aplicar).grid(row=1, column=0, padx=5, pady=5)
+tk.Button(tool_bar, text="Sal y pimienta",command=lambda:aplicarsp()).grid(row=1, column=0, padx=5, pady=5)
 tk.Button(tool_bar, text="Gasussiano",command=aplicar).grid(row=2, column=0, padx=5, pady=5)
-tk.Button(tool_bar, text="Restaurar 1",command=aplicar).grid(row=1, column=1, padx=5, pady=5)
+tk.Button(tool_bar, text="Restaurar Sal y pimienta",command=lambda:filtrosp()).grid(row=1, column=1, padx=5, pady=5)
 tk.Button(tool_bar, text="Restarurar 2",command=aplicar).grid(row=2, column=1, padx=5, pady=5)
 
 root.bind("<Return>", loadPreview)
-root.bind("<Return>", aplicar)
+root.bind("<Return>", aplicarsp)
+root.bind("<Return>", filtrosp)
 root.mainloop()
--- filterfunctions.py
+++ filterfunctions.py
@@ -9,6 +9,7 @@ def gaussNoise(img):
     var = 1000
     sigma = var ** 0.5
     gaussian = np.random.normal(mean, sigma, (img.shape[0],img.shape[1])) 
+    gaussian = gaussian.astype(int)
     noisy_image = np.zeros(img.shape, np.float32)
     noisy_image = img + gaussian
     cv2.normalize(noisy_image, noisy_image, 0, 255, cv2.NORM_MINMAX, dtype=-1)
@@ -21,7 +22,7 @@ def gaussNoiseFilter(img):
     return processed_image
 
 def saltPepperFilter(img):
-    processed_image = cv2.medianBlur(image, 3)
+    processed_image = cv2.medianBlur(img, 5)
     return processed_image
 def saltPepper(img):
     if(len(img.shape)!= 2):
@@ -34,6 +35,30 @@ def saltPepper(img):
         x_coord=random.randint(0, col - 1)
         img[y_coord][x_coord] = 255
     number_of_pixels = random.randint(300 , 10000)
+    for i in range(number_of_pixels):
+        y_coord=random.randint(0, row - 1)
+        x_coord=random.randint(0, col - 1)
+        img[y_coord][x_coord] = 0
+    return noisy_img
+
+def salt(img):
+    if(len(img.shape)!= 2):
+        img = img[:,:,0]
+    row , col = img.shape
+    noisy_img = np.copy(img)
+    number_of_pixels = random.randint(300, 10000)
+    for i in range(number_of_pixels):
+        y_coord=random.randint(0, row - 1)
+        x_coord=random.randint(0, col - 1)
+        img[y_coord][x_coord] = 255
+    return noisy_img
+
+def pepper(img):
+    if(len(img.shape)!= 2):
+        img = img[:,:,0]
+    row , col = img.shape
+    noisy_img = np.copy(img)
+    number_of_pixels = random.randint(300, 10000)
     for i in range(number_of_pixels):
         y_coord=random.randint(0, row - 1)
         x_coord=random.randint(0, col - 1)
